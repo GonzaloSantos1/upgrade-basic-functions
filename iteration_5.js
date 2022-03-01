7e8a488b0ae87b89c26ecb3d094d67df08ca9848
Gonzalo Santos <96088669+GonzaloSantos1@users.noreply.github.com>
2022-03-01 16:08:05 +0100

    Update iteration_5.js

--- iteration_5.js
+++ iteration_5.js
@@ -5,11 +5,11 @@ const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
 function averageWord(param) {
   let sum = 0;
 
-  for (i = 0; i < mixedElements.length; i++) {
-    if (typeof mixedElements[i] === 'string') {
-      sum += mixedElements[i].length;
+  for (i = 0; i < param.length; i++) {
+    if (typeof param[i] === 'string') {
+      sum += param[i].length;
     } else {
-      sum += mixedElements[i];
+      sum += param[i];
     }
   }
 
